4107ee1dd312ba19819a42f364b2ccf4d9480281
CJ Pais <cj@cjpais.com>
2025-02-07 11:30:41 -0800

    Google Cloud SQL.

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,22 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 
-const db = drizzle(process.env.DATABASE_URL!);
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host:
+    process.env.NODE_ENV === "production"
+      ? process.env.INSTANCE_CONNECTION_NAME
+      : process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  ssl: {
+    // @ts-expect-error require true is not recognized by @types/pg, but does exist on pg
+    require: true,
+    rejectUnauthorized: false,
+  },
+});
+
+const db = drizzle({ client: pool });
 
 export default db;
