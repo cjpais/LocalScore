990b3052b5fe8c03e03460ce59544d4f9e4d2733
CJ Pais <cj@cjpais.com>
2025-03-20 10:17:15 -0700

    test no cache for page

--- src/pages/result/[id].tsx
+++ src/pages/result/[id].tsx
@@ -168,11 +168,6 @@ const Page: React.FC<{ result: DetailedRun | null }> = ({ result }) => {
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  context.res.setHeader(
-    "Cache-Control",
-    "public, max-age=15768000, stale-while-revalidate=31536000"
-  );
-
   const startTime = Date.now();
 
   const { id } = context.query;
